e8af2145dbbf5696ecc4c4004ab6cfa77e5e1283
Abhoy Gorai <abhoygorai04@gmail.com>
2023-03-12 10:34:20 +0530

    registration flow done

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,34 +1,111 @@
 import "../styles/Register.css";
 import Navbar from "./Navbar";
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 function Register() {
+  const [user, setUser] = useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [gender, setGender] = useState();
+  const [age, setAge] = useState();
+  const [weight, setWeight] = useState();
+  const [password, setPassword] = useState();
+
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    setUser({
+      name: name,
+      email: email,
+      gender: gender,
+      age: age,
+      weight: weight,
+      password: password,
+    });
+
+    try {
+      // console.log(data)
+      const apidata = await axios.post("http://localhost:4000/api/v1/auth/register",
+        user,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (apidata.status === 200) {
+        console.log(apidata)
+      }
+      // console.log(apidata);
+    } catch (error) {
+      // console.log(apidata);
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Navbar />
       <div className="register-page-main-portion">
         <form className="register-page-details-form">
-          <p className="label" style={{fontSize: '2rem'}}>Register</p>
+          <p className="label" style={{ fontSize: "2rem" }}>
+            Register
+          </p>
           <p className="label">Name</p>
-          <input type="text" />
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
           <p className="label">Email</p>
-          <input type="email" />
+          <input
+            required
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <div className="register-page-small-div">
             <div className="">
               <p className="label">Gender</p>
-              <input type="text" />
+              <input
+                required
+                type="text"
+                onChange={(e) => setGender(e.target.value)}
+              />
             </div>
             <div className="">
               <p className="label">Age</p>
-              <input type="text" />
+              <input
+                required
+                type="text"
+                onChange={(e) => setAge(e.target.value)}
+              />
             </div>
           </div>
           <p className="label">Body Weight</p>
-          <input type="text" />
+          <input
+            required
+            type="text"
+            onChange={(e) => setWeight(e.target.value)}
+          />
+          <p className="label">Password</p>
+          <input
+            required
+            type="text"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button id="register-page-submit-button">Submit</button>
+          <button id="register-page-submit-button" onClick={submitForm}>
+            Submit
+          </button>
         </form>
-        <img src="page-image.png" className="register-page-hero-image" />
+        <img
+          src="page-image.png"
+          alt="food"
+          className="register-page-hero-image"
+        />
       </div>
     </>
   );
